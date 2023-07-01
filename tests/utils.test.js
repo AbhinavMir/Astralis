37a5531d01aa0fd315234dfca3cdff0007dfdb8d
Abhinav Srivastava <atg271@gmail.com>
2023-07-02 05:29:16 +0530

    wip

---
+++ tests/utils.test.js
@@ -0,0 +1,111 @@
+// Import the required dependencies and modules
+const {
+    airdropToken,
+    Elusiv,
+    getMintAccount,
+    TokenType,
+    SEED_MESSAGE
+  } = require("@elusiv/sdk");
+  const { Keypair, Connection, Cluster } = require("@solana/web3.js");
+  const { sign } = require('@noble/ed25519');
+  
+  // Import the functions to be tested
+  const {
+    generateKey,
+    getParams,
+    topup,
+    send,
+    onramp_fiat
+  } = require('../utils/utils'); // Replace 'your-module-name' with the actual name of the module containing the functions
+  
+  // Mock the necessary dependencies
+  jest.mock('@elusiv/sdk');
+  jest.mock('@solana/web3.js');
+  jest.mock('@noble/ed25519');
+  
+  describe('Your Test Suite', () => {
+    // Mock the necessary objects
+    const mockKeyPair = {
+      secretKey: Buffer.from('your-secret-key'),
+      publicKey: Buffer.from('your-public-key')
+    };
+    const mockElusivInstance = {
+      buildTopUpTx: jest.fn().mockResolvedValue({ tx: { partialSign: jest.fn() } }),
+      sendElusivTx: jest.fn().mockResolvedValue('Top-up transaction sent successfully.'),
+      buildSendTx: jest.fn().mockResolvedValue({}),
+      sendElusivTx: jest.fn().mockResolvedValue('Send transaction sent successfully.')
+    };
+  
+    beforeEach(() => {
+      // Clear mock function calls and reset any necessary mocks
+      jest.clearAllMocks();
+    });
+  
+    describe('generateKey', () => {
+      it('should generate a key pair', async () => {
+        const keyPair = await generateKey();
+  
+        expect(keyPair).toHaveProperty('secretKey');
+        expect(keyPair).toHaveProperty('publicKey');
+      });
+    });
+  
+    describe('getParams', () => {
+      it('should return Elusiv instance, key pair, and connection', async () => {
+        const mockConnection = new Connection();
+  
+        Keypair.generate.mockReturnValueOnce(mockKeyPair);
+        sign.mockResolvedValueOnce(Buffer.from('your-signed-seed'));
+        Elusiv.getElusivInstance.mockResolvedValueOnce(mockElusivInstance);
+        Connection.mockImplementationOnce(() => mockConnection);
+  
+        const { elusiv, keyPair, conn } = await getParams();
+  
+        expect(keyPair).toEqual(mockKeyPair);
+        expect(Elusiv.getElusivInstance).toHaveBeenCalledWith(
+          Buffer.from('your-signed-seed'),
+          Buffer.from('your-public-key'),
+          mockConnection,
+          'devnet'
+        );
+        expect(conn).toBe(mockConnection);
+      });
+    });
+  
+    describe('topup', () => {
+      it('should build and send a top-up transaction', async () => {
+        const amount = 100;
+        const tokenType = TokenType.ETH;
+  
+        await topup(mockElusivInstance, mockKeyPair, amount, tokenType);
+  
+        expect(mockElusivInstance.buildTopUpTx).toHaveBeenCalledWith(amount, tokenType);
+        expect(mockElusivInstance.sendElusivTx).toHaveBeenCalled();
+      });
+    });
+  
+    describe('send', () => {
+      it('should build and send a send transaction', async () => {
+        const recipient = 'your-recipient-address';
+        const amount = 50;
+        const tokenType = TokenType.SOL;
+  
+        await send(mockElusivInstance, recipient, amount, tokenType);
+  
+        expect(mockElusivInstance.buildSendTx).toHaveBeenCalledWith(amount, recipient, tokenType);
+        expect(mockElusivInstance.sendElusivTx).toHaveBeenCalled();
+      });
+    });
+  
+    describe('onramp_fiat', () => {
+      it('should return null', async () => {
+        const amount = 500;
+        const tokenType = TokenType.USD;
+  
+        const result = await onramp_fiat(amount, tokenType);
+  
+        expect(result).toBeNull();
+      });
+    });
+  });
+  
